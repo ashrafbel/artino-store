9140690f78465af68c8e69fcead7050e8fb3ffed
ashrafbel <ashrafmus2@gmail.com>
2025-01-26 17:34:25 -0800

    Modified header component with additional  comments

--- artino-store/src/Components/Header/Header.jsx
+++ artino-store/src/Components/Header/Header.jsx
@@ -5,9 +5,12 @@ import Navicon from "./Navicon/Navicon";
 import { useNavigate } from "react-router-dom";
 
 const Header = () =>{
+    // Initialize the navigate function for navigating between pages
     const navigate = useNavigate();
+    
+    // Function to navigate to the "allproducts" page when Products link is clicked
     const handleProducts = () => {
-        navigate("/allproducts")
+        navigate("/allproducts") // Navigate to the products page
     }
     return(
         <div className="header-items">
